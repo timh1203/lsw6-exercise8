5b9e5edcaba120272c3e75296196e5266287d561
Timothy Hoang <timh1203@gmail.com>
2018-08-29 18:03:35 -0500

    Add input change event

--- src/index.js
+++ src/index.js
@@ -4,13 +4,23 @@ import ReactDOM from "react-dom";
 import "./styles.css";
 
 class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      message: ''
+    }
+  }
+
   handleSingleClick = () => alert('Single Click Triggered')
-
   handleDoubleClick = () => alert('DoubleClick Triggered')
-  
   handleMouseEnter = () => alert('Mouse Enter Triggered')
-
   handleMouseLeave = () => alert('Mouse Leave Triggered')
+
+  handleInputChange = (e) => {
+    e.preventDefault()
+    const message = e.target.value
+    this.setState({ message })
+  }
 
   render() {
     return (
@@ -21,7 +31,7 @@ class App extends React.Component {
         <button onDoubleClick={this.handleDoubleClickEvent}>Double Click Handler</button>
         <div onMouseEnter={this.handleMouseEnter}>Mouse Enter</div>
         <div onMouseLeave={this.handleMouseLeave}>Mouse Leave</div>
-        <input placeholder="Change my input" />
+        <input type="text" placeholder="Change my input" onChange={this.handleInputChange} />
       </div>
     );
   }
