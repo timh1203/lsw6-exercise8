b02e5884ef67ade6aaeb364094f246d1e84f8b06
Timothy Hoang <timh1203@gmail.com>
2018-08-29 17:54:22 -0500

    Add single and double click handlers

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import ReactDOM from "react-dom";
 import "./styles.css";
 
 class App extends React.Component {
+  handleSingleClickEvent = () => alert('Single Click Event Triggered')
+
+  handleDoubleClickEvent = () => alert('DoubleClick Event Triggered')
   render() {
     return (
       <div className="App">
         <h1>Hello Handlers</h1>
         <h2>Lets build out some handler functions.</h2>
-        <button>Click Handler Demo</button>
-        <button>Double Click Handler</button>
+        <button onClick={this.handleSingleClickEvent}>Click Handler Demo</button>
+        <button onDoubleClick={this.handleDoubleClickEvent}>Double Click Handler</button>
         <div>Mouse Leave</div>
         <input placeholder="Change my input" />
       </div>
